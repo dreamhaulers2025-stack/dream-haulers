d2219e0944249eac00d3d8790cf2968f0e2d3738
Mandeep Sharma <mandeep.sharma@circana.com>
2025-09-20 00:03:52 +0530

    push the changes for icons

--- spa-react17/src/components/Header.tsx
+++ spa-react17/src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.jpg';
 
 export default function Header() {
   return (
     <header className="site-header">
       <div className="container header-inner">
         <Link to="/" className="brand">
-          <img src="/logo.jpg" alt="Dream Haulers" className="logo" />
+          <img src={logo} alt="Dream Haulers" className="logo" />
         </Link>
         <nav className="nav">
           <Link to="/about">About</Link>
